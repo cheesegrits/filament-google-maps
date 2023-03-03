6085282e043467c0a156f199c7ff791375cf19c5
Hugh Messenger <hugh.messenger@gmail.com>
2023-03-03 16:24:05 -0600

    Temporarily comment out getHintAction

--- resources/views/fields/filament-google-geocomplete.blade.php
+++ resources/views/fields/filament-google-geocomplete.blade.php
@@ -13,7 +13,7 @@
         :label-sr-only="$isLabelHidden()"
         :helper-text="$getHelperText()"
         :hint="$getHint()"
-        :hint-action="$getHintAction()"
+{{--        :hint-action="$getHintAction()"--}}
         :hint-color="$getHintColor()"
         :hint-icon="$getHintIcon()"
         :required="$isRequired()"
