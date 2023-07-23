49c613d7d4db8e42753625bb8d2345d2480dbaaf
Hugh Messenger <hugh.messenger@gmail.com>
2023-07-23 14:59:47 -0500

    Bump dependencies for LW 3, remove CanPretendToBeAFile import

--- src/Controllers/FilamentGoogleMapAssets.php
+++ src/Controllers/FilamentGoogleMapAssets.php
@@ -4,12 +4,9 @@ namespace Cheesegrits\FilamentGoogleMaps\Controllers;
 
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Response;
-use Livewire\Controllers\CanPretendToBeAFile;
 
 class FilamentGoogleMapAssets
 {
-    use CanPretendToBeAFile;
-
     public function __invoke($file)
     {
         if (pathinfo($file, PATHINFO_EXTENSION) === 'png') {
