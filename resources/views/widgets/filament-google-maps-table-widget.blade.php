03d9435ba86379dcaf376a43afb6c69ac21c307e
Hugh Messenger <hugh.messenger@gmail.com>
2023-09-11 19:48:08 -0500

    Add mt-2 to table section in MapTable widget, to separate the two sections a little

--- resources/views/widgets/filament-google-maps-table-widget.blade.php
+++ resources/views/widgets/filament-google-maps-table-widget.blade.php
@@ -4,7 +4,7 @@
 @endphp
 
 <x-filament-widgets::widget class="filament-google-maps-widget">
-    <x-filament::card>
+    <x-filament::section>
         @if ($heading || $filters)
             <div class="flex items-center justify-between gap-8">
                 @if ($heading)
@@ -15,12 +15,12 @@
 
                 @if ($filters)
                     <select
-                            wire:model="filter"
-                            @class([
-                                'text-gray-900 border-gray-300 block h-10 transition duration-75 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500',
-                                'dark:bg-gray-700 dark:border-gray-600 dark:text-gray-200 dark:focus:border-primary-500' => config('filament.dark_mode'),
-                            ])
-                            wire:loading.class="animate-pulse"
+                        wire:model="filter"
+                        @class([
+                            'focus:border-primary-500 focus:ring-primary-500 block h-10 rounded-lg border-gray-300 text-gray-900 shadow-sm transition duration-75 focus:ring-1 focus:ring-inset',
+                            'dark:focus:border-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-200' => config('filament.dark_mode'),
+                        ])
+                        wire:loading.class="animate-pulse"
                     >
                         @foreach ($filters as $value => $label)
                             <option value="{{ $value }}">
@@ -32,38 +32,37 @@
             </div>
         @endif
 
-        <div class=""
-                wire:key="{{ rand() }}"
-                x-ignore
-                ax-load
-                ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-google-maps-widget', 'cheesegrits/filament-google-maps') }}"
-                x-data="filamentGoogleMapsWidget({
+        <div
+            class=""
+            wire:key="{{ rand() }}"
+            x-ignore
+            ax-load
+            ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-google-maps-widget', 'cheesegrits/filament-google-maps') }}"
+            x-data="filamentGoogleMapsWidget({
                     cachedData: {{  json_encode($this->getCachedData()) }},
                     config: {{ $this->getMapConfig()}},
                     mapEl: $refs.map,
 {{--                    mapFilterIds: {{ $this->mapIsFilter() ? 'wire:@entangle("mapFilterIds")' : null}}--}}
                 })"
-                wire:ignore
-                @if ($maxHeight = $this->getMaxHeight())
-                    style=" max-height: {{ $maxHeight }}"
-                @endif
-            >
+            wire:ignore
+            @if ($maxHeight = $this->getMaxHeight())
+                style=" max-height: {{ $maxHeight }}"
+            @endif
+        >
             <div
-                @if($this->mapIsFilter())
-                    wire:@entangle('mapFilterIds')
+                @if ($this->mapIsFilter())
+                    wire: @entangle('mapFilterIds')
                 @endif
-                        
                 wire:ignore
-                id="map-{{ $this->getMapId() }}" x-ref="map" class="w-full"
-                style="min-height: 50vh; z-index: 1 !important;"
-                >
-            </div>
-
+                id="map-{{ $this->getMapId() }}"
+                x-ref="map"
+                class="w-full"
+                style="min-height: 50vh; z-index: 1 !important"
+            ></div>
         </div>
-    </x-filament::card>
+    </x-filament::section>
 
-    <x-filament::card>
+    <x-filament::section class="mt-2">
         {{ $this->table }}
-    </x-filament::card>
-
+    </x-filament::section>
 </x-filament-widgets::widget>
