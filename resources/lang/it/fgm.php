27702c6b3fa51ab9dfa0cf836f397eb1f354e76c
becker <beckermaxime84@gmail.com>
2024-02-08 14:54:16 +0100

    add lang it

---
+++ resources/lang/it/fgm.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    'radius_filter' => [
+        'address' => 'Indirizzo',
+        'miles' => 'Miglia',
+        'kilometers' => 'Chilometri',
+        'distance' => 'Distanza',
+        'unit' => 'Unità',
+        'title' => 'Ricerca per Raggio',
+        'indicate' => 'Entro :radius :units da :address',
+    ],
+    'map_is_filter' => [
+        'indicate' => 'mostrando :count indicatori nella vista',
+    ],
+    'static_map_action' => [
+        'modal' => [
+            'heading' => 'Scarica Mappa Statica',
+            'label' => 'Fallo!',
+            'success' => 'Successo',
+        ],
+        'button' => [
+            'label' => 'Crea Mappa Statica',
+        ],
+    ],
+    'widget_map_action' => [
+        'modal' => [
+            'heading' => 'Mostra Mappa',
+            'label' => 'Fallo!',
+            'success' => 'Successo',
+        ],
+        'button' => [
+            'label' => 'Fai Qualcosa',
+        ],
+    ],
+    'radius_action' => [
+        'modal' => [
+            'heading' => 'Mostra Mappa',
+            'label' => 'Fallo!',
+            'success' => 'Successo',
+        ],
+        'button' => [
+            'label' => 'Raggio',
+        ],
+    ],
+    'goto_action' => [
+        'button' => [
+            'label' => 'Vai a',
+        ],
+    ],
+    'geolocate' => [
+        'label' => 'Imposta Posizione Corrente',
+    ],
+];
