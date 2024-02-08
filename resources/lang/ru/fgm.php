ea47f7268e344de62d36456dd07deef5d4867bfa
becker <beckermaxime84@gmail.com>
2024-02-08 14:58:49 +0100

    add lang ru

---
+++ resources/lang/ru/fgm.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    'radius_filter' => [
+        'address' => 'Адрес',
+        'miles' => 'Мили',
+        'kilometers' => 'Километры',
+        'distance' => 'Расстояние',
+        'unit' => 'Единица измерения',
+        'title' => 'Поиск по радиусу',
+        'indicate' => 'В пределах :radius :units от :address',
+    ],
+    'map_is_filter' => [
+        'indicate' => 'показано :count маркеров на карте',
+    ],
+    'static_map_action' => [
+        'modal' => [
+            'heading' => 'Скачать статичную карту',
+            'label' => 'Сделай это!',
+            'success' => 'Успех',
+        ],
+        'button' => [
+            'label' => 'Создать статичную карту',
+        ],
+    ],
+    'widget_map_action' => [
+        'modal' => [
+            'heading' => 'Показать карту',
+            'label' => 'Сделай это!',
+            'success' => 'Успех',
+        ],
+        'button' => [
+            'label' => 'Сделать что-то',
+        ],
+    ],
+    'radius_action' => [
+        'modal' => [
+            'heading' => 'Показать карту',
+            'label' => 'Сделай это!',
+            'success' => 'Успех',
+        ],
+        'button' => [
+            'label' => 'Радиус',
+        ],
+    ],
+    'goto_action' => [
+        'button' => [
+            'label' => 'Перейти к',
+        ],
+    ],
+    'geolocate' => [
+        'label' => 'Установить текущее местоположение',
+    ],
+];
