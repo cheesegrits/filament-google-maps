114b7fd136e215ac786ee37c726b79797bf2fd49
becker <beckermaxime84@gmail.com>
2024-02-08 15:01:20 +0100

    add lang nl

---
+++ resources/lang/nl/fgm.php
@@ -0,0 +1,54 @@
+<?php
+return [
+    'radius_filter' => [
+        'address' => 'Adres',
+        'miles' => 'Mijlen',
+        'kilometers' => 'Kilometers',
+        'distance' => 'Afstand',
+        'unit' => 'Eenheid',
+        'title' => 'Zoeken op Radius',
+        'indicate' => 'Binnen :radius :units van :address',
+    ],
+    'map_is_filter' => [
+        'indicate' => 'toont :count markeringen in beeld',
+    ],
+    'static_map_action' => [
+        'modal' => [
+            'heading' => 'Statische Kaart Downloaden',
+            'label' => 'Doe het!',
+            'success' => 'Succes',
+        ],
+        'button' => [
+            'label' => 'Maak Statische Kaart',
+        ],
+    ],
+    'widget_map_action' => [
+        'modal' => [
+            'heading' => 'Kaart Weergeven',
+            'label' => 'Doe het!',
+            'success' => 'Succes',
+        ],
+        'button' => [
+            'label' => 'Doe Iets',
+        ],
+    ],
+    'radius_action' => [
+        'modal' => [
+            'heading' => 'Kaart Weergeven',
+            'label' => 'Doe het!',
+            'success' => 'Succes',
+        ],
+        'button' => [
+            'label' => 'Radius',
+        ],
+    ],
+    'goto_action' => [
+        'button' => [
+            'label' => 'Ga Naar',
+        ],
+    ],
+    'geolocate' => [
+        'label' => 'Huidige Locatie Instellen',
+    ],
+];
+
