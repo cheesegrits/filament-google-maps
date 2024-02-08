ceddeb4d7c16c18a7fc07c426bb7e6c464ed3af5
becker <beckermaxime84@gmail.com>
2024-02-08 15:08:38 +0100

    add lang pt_PT

---
+++ resources/lang/pt_PT/fgm.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    'radius_filter' => [
+        'address' => 'Endereço',
+        'miles' => 'Milhas',
+        'kilometers' => 'Quilômetros',
+        'distance' => 'Distância',
+        'unit' => 'Unidade',
+        'title' => 'Pesquisa por Raio',
+        'indicate' => 'Dentro de :radius :units de :address',
+    ],
+    'map_is_filter' => [
+        'indicate' => 'mostrando :count marcadores na vista',
+    ],
+    'static_map_action' => [
+        'modal' => [
+            'heading' => 'Baixar Mapa Estático',
+            'label' => 'Faça isso!',
+            'success' => 'Sucesso',
+        ],
+        'button' => [
+            'label' => 'Criar Mapa Estático',
+        ],
+    ],
+    'widget_map_action' => [
+        'modal' => [
+            'heading' => 'Exibir Mapa',
+            'label' => 'Faça isso!',
+            'success' => 'Sucesso',
+        ],
+        'button' => [
+            'label' => 'Faça Algo',
+        ],
+    ],
+    'radius_action' => [
+        'modal' => [
+            'heading' => 'Exibir Mapa',
+            'label' => 'Faça isso!',
+            'success' => 'Sucesso',
+        ],
+        'button' => [
+            'label' => 'Raio',
+        ],
+    ],
+    'goto_action' => [
+        'button' => [
+            'label' => 'Ir Para',
+        ],
+    ],
+    'geolocate' => [
+        'label' => 'Definir Localização Atual',
+    ],
+];
