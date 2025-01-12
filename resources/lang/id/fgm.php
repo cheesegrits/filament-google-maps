4e7d47d4969cd6940d4f159c1fce411e4dd4183a
Wildan <cuinc99@gmail.com>
2025-01-12 16:45:18 +0800

    feat: add Indonesian language

---
+++ resources/lang/id/fgm.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    'radius_filter' => [
+        'address'    => 'Alamat',
+        'miles'      => 'Mil',
+        'kilometers' => 'Kilometer',
+        'distance'   => 'Jarak',
+        'unit'       => 'Unit',
+        'title'      => 'Pencarian Radius',
+        'indicate'   => 'Dalam radius :radius :units dari :address',
+    ],
+    'map_is_filter' => [
+        'indicate' => 'menampilkan :count penanda dalam tampilan',
+    ],
+    'static_map_action' => [
+        'modal' => [
+            'heading' => 'Unduh Peta Statis',
+            'label'   => 'Lakukan!',
+            'success' => 'Berhasil',
+        ],
+        'button' => [
+            'label' => 'Buat Peta Statis',
+        ],
+    ],
+    'widget_map_action' => [
+        'modal' => [
+            'heading' => 'Tampilkan Peta',
+            'label'   => 'Lakukan!',
+            'success' => 'Berhasil',
+        ],
+        'button' => [
+            'label' => 'Lakukan Sesuatu',
+        ],
+    ],
+    'radius_action' => [
+        'modal' => [
+            'heading' => 'Tampilkan Peta',
+            'label'   => 'Lakukan!',
+            'success' => 'Berhasil',
+        ],
+        'button' => [
+            'label' => 'Radius',
+        ],
+    ],
+    'goto_action' => [
+        'button' => [
+            'label' => 'Pergi Ke',
+        ],
+    ],
+    'geolocate' => [
+        'label' => 'Atur Lokasi Saat Ini',
+    ],
+];
