3468c60772b5a3d9bf2da506ca5438bc02868a13
CHARLIE ETIENNE <ce@web-nancy.fr>
2026-01-23 12:36:23 +0100

    Replace `getCachedForms` with `getCachedSchemas` in `InteractsWithMaps`.

--- src/Concerns/InteractsWithMaps.php
+++ src/Concerns/InteractsWithMaps.php
@@ -43,7 +43,7 @@ trait InteractsWithMaps
 
     public function placeUpdatedUsing(string $statePath, array $results): bool
     {
-        foreach ($this->getCachedForms() as $form) {
+        foreach ($this->getCachedSchemas() as $form) {
             if ($this->placeUpdated($form, $statePath, $results)) {
                 return true;
             }
